cfa9b2fae9385a7eb747a2b539094adc613285ed
HyungjinJeon <qpdjgjsl@gmail.com>
2019-05-28 22:41:09 -0700

    I Am Rich APP

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by 전형진 on 28/05/2019.
+//  Copyright © 2019 Hyungjin Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
